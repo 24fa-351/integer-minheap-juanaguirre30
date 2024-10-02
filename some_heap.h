a59f0eed6067aea67f6fb997da235830874e1f40
juanaguirre30 <juanaguirre30@csu.fullerton.edu>
2024-10-02 10:03:46 -0700

    1st submission

--- some_heap.h
+++ some_heap.h
@@ -5,21 +5,21 @@
 // a min heap
 
 typedef unsigned long long heap_key_t;
-typedef void* heap_value_t;
+typedef void* heap_value_t; // generic pointer used to store key values
 #define HEAP_KEY_FORMAT "%llu"
 
-typedef struct {
-    heap_key_t key;
-    heap_value_t value;
+typedef struct { // single node in heap
+    heap_key_t key; // key in heap (NOT POSITION)
+    heap_value_t value; // pointer to associated value
 } heap_node_t;
 
 typedef struct {
-    heap_node_t *data;
+    heap_node_t *data; // pointer to an array of heap_node_t structures (nodes), first element in array
     int size;
     int capacity;
 } heap_t;
 
-heap_t *heap_create(int capacity);
+heap_t *heap_create(int capacity); // returns pointer to the heap
 
 void heap_free(heap_t *heap);
 
